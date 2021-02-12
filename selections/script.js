06d937e40d9a255e5b6909ed8a20f6e0da38753c
iromashko <iromashko@me.com>
2021-02-12 09:30:49 +0300

    part 4

--- selections/script.js
+++ selections/script.js
@@ -26,3 +26,35 @@ allCircles.selectAll(function (d, i, n) {
 const oddCircles = allCircles.filter(':nth-child(odd)');
 
 console.log(oddCircles);
+
+let allSvg1 = d3.selectAll('#svg1 circle');
+console.log(allSvg1);
+let allSvg2 = d3.selectAll('#svg2 circle');
+console.log(allSvg1);
+
+const allOdd = allSvg1.select(function (d, i, n) {
+  if (i % 2 === 0) {
+    this.style.fill = 'indianred';
+    return this;
+  } else {
+    return null;
+  }
+});
+const allEven = allSvg1.select(function (d, i, n) {
+  if (i % 2 !== 0) {
+    this.style.fill = 'beige';
+    return this;
+  } else {
+    return null;
+  }
+});
+
+console.log(allEven);
+
+const all = allOdd.merge(allEven);
+console.log(all);
+
+const firstCircle = d3.select('svg').selectChild();
+console.log(firstCircle);
+const secondCircle = d3.select('svg').selectChild(':nth-child(2)');
+console.log(secondCircle);
